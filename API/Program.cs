70c964afdf06f7148fab6d3128b55f2951b4796d
reskandari <rezaeskandari.ammori@yahoo.com>
2024-05-13 09:17:57 +0200

    added swagger configurations to get a token backe from the api for individual user

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,13 @@
+using System.Text;
 using API.Data;
 using API.Entities;
 using API.Middleware;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +15,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+        var jwtSecuritySchema =new OpenApiSecurityScheme 
+        {
+            BearerFormat ="JWT",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = JwtBearerDefaults.AuthenticationScheme,
+            Description = "Put Bearer + your token in the box below",
+            Reference = new OpenApiReference 
+            {
+                Id = JwtBearerDefaults.AuthenticationScheme,
+                Type = ReferenceType.SecurityScheme
+            }
+        };
+
+        c.AddSecurityDefinition(jwtSecuritySchema.Reference.Id, jwtSecuritySchema);
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+            jwtSecuritySchema, Array.Empty<string>()
+            }
+        });
+    }
+);
 builder.Services.AddDbContext<CenterMarketContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // for to allowing frontend to make the request to the api 
@@ -26,7 +54,22 @@ builder.Services.AddIdentityCore<User>(opt => {
 })
 .AddRoles<IdentityRole>()
 .AddEntityFrameworkStores<CenterMarketContext>();
-builder.Services.AddAuthentication();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(opt => 
+{
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false ,
+        ValidateAudience = false,
+        // api cheking life time of the token
+        ValidateLifetime =true ,
+        // chking validity of the token based of the token signuture 
+        ValidateIssuerSigningKey =true,
+        // decript the key
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.
+        GetBytes(builder.Configuration["JWTSettings:TokenKey"]))
+    };
+});
 builder.Services.AddAuthentication();
 builder.Services.AddScoped<TokenService>();
 
@@ -37,7 +80,10 @@ app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => 
+    {
+        c.ConfigObject.AdditionalItems.Add("persistAuthorization","true");
+    });
 }
 
 // allowing any http method and any header but just from http://localhost:3000" 
@@ -45,6 +91,7 @@ app.UseCors(opt => opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().With
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
