a1984e0385a9b82ad57edaf746b0a2638a05c491
reskandari <rezaeskandari.ammori@yahoo.com>
2024-05-16 18:21:31 +0200

    added users orders address entitys in databse

--- API/Data/CenterMarketContext.cs
+++ API/Data/CenterMarketContext.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Entities;
+using API.Entities.OrderAggregate;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class CenterMarketContext : IdentityDbContext<User>
+    public class CenterMarketContext : IdentityDbContext<User,Role,int>
     {
         public CenterMarketContext(DbContextOptions options) : base(options)
         {
@@ -17,15 +18,23 @@ namespace API.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Basket> Baskets{get; set;}
+        public DbSet<Order> Orders {get; set;}
 
         // seeding data to the database overriding from the method of DbContext
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            builder.Entity<IdentityRole>()
+
+            builder.Entity<User>()
+            .HasOne(a => a.Address)
+            .WithOne()
+            .HasForeignKey<UserAddress>(a => a.Id)
+            .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<Role>()
             .HasData(
-                new IdentityRole{Name = "Member", NormalizedName = "MEMBER"},
-                new IdentityRole{Name = "Admin", NormalizedName = "ADMIN"}
+                new Role{Id= 1 ,Name = "Member", NormalizedName = "MEMBER"},
+                new Role{Id= 2 ,Name = "Admin", NormalizedName = "ADMIN"}
             );
         }
     }
